8ab8bf8980b5ffd684f1da80a79b52830d79dafe
Krish-2512 <krish12252005@gmail.com>
2026-04-01 19:48:09 +0530

    changed build path for redis/upstash

--- models/Question.js
+++ models/Question.js
@@ -34,7 +34,6 @@ const QuestionSchema = new mongoose.Schema({
   timestamps: true 
 });
 
-QuestionSchema.index({ title: 1 }, { unique: true });
 QuestionSchema.index({ category: 1, difficulty: 1, createdAt: -1 });
 QuestionSchema.index({ status: 1, createdAt: -1 });
 QuestionSchema.index({ companyTags: 1 });
